976b5c0d254435fb37d0274963bed51e733a715a
jianwei.qin <jianwei.qin@shuyun.com>
2021-01-06 10:06:18 +0800

    新增全局id

---
+++ core/id.py
@@ -0,0 +1,19 @@
+#
+#    Copyright (c) 2007-2009 Corey Goldberg (corey@goldb.org)
+#    License: GNU GPLv3
+#
+#    This file is part of Pylot.
+#    
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.  See the GNU General Public License 
+#    for more details.
+#
+
+index = -1
+def id():
+    global index
+    index = index + 1
+    return index
+    
